31d92ef09d0a03a863868e53b6436889a1954128
Sahilgit1 <162616651+Sahilgit1@users.noreply.github.com>
2024-03-12 02:42:41 +0530

    Update urls.py

--- siteapp/urls.py
+++ siteapp/urls.py
@@ -12,5 +12,4 @@ path('homee', views.homee, name='home'),
 
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
